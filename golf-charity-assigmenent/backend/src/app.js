bc01cb273638d07b0972e8b2345340ab3e70a354
mayur2410-tech <mayurwaykar9@gmail.com>
2026-03-26 23:17:02 +0530

    add app.js

---
+++ golf-charity-assigmenent/backend/src/app.js
@@ -0,0 +1,53 @@
+const express = require("express");
+const cors = require("cors");
+const helmet = require("helmet");
+const morgan = require("morgan");
+const rateLimit = require("express-rate-limit");
+const path = require("path");
+
+const authRoutes = require("./routes/authRoutes");
+const userRoutes = require("./routes/userRoutes");
+const scoreRoutes = require("./routes/scoreRoutes");
+const drawRoutes = require("./routes/drawRoutes");
+const subscriptionRoutes = require("./routes/subscriptionRoutes");
+const charityRoutes = require("./routes/charityRoutes");
+const winnerRoutes = require("./routes/winnerRoutes");
+const { handleWebhook } = require("./controllers/subscriptionController");
+
+const { notFound, errorHandler } = require("./middlewares/errorMiddleware");
+
+const app = express();
+
+app.use(helmet());
+app.use(cors());
+app.use(morgan("dev"));
+// app.use(
+//   rateLimit({
+//     windowMs: 15 * 60 * 1000,
+//     max: 300
+//   })
+// );
+
+app.get("/health", (req, res) => {
+  res.status(200).json({ status: "ok" });
+});
+
+app.use("/api/subscriptions/webhook", express.raw({ type: "application/json" }));
+app.use("/api/webhook", express.raw({ type: "application/json" }));
+app.use(express.json());
+app.use("/uploads", express.static(path.join(process.cwd(), "uploads")));
+
+app.use("/api/auth", authRoutes);
+app.use("/api/user", userRoutes);
+app.use("/api/scores", scoreRoutes);
+app.use("/api/draw", drawRoutes);
+app.use("/api/subscriptions", subscriptionRoutes);
+app.use("/api/subscribe", subscriptionRoutes);
+app.post("/api/webhook", handleWebhook);
+app.use("/api/charities", charityRoutes);
+app.use("/api/winners", winnerRoutes);
+
+app.use(notFound);
+app.use(errorHandler);
+
+module.exports = app;
